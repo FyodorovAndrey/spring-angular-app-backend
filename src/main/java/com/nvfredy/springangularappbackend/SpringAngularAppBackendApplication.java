a079bc815af67e9dbf856caf625e0880e7447192
Andrey Fyodorov <Andrey@Andreys-MacBook-Pro.local>
2021-04-09 20:26:33 +0500

    Initial commit

---
+++ src/main/java/com/nvfredy/springangularappbackend/SpringAngularAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nvfredy.springangularappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularAppBackendApplication.class, args);
+    }
+
+}
